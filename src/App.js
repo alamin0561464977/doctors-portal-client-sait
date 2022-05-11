94a06ff9974d781d31594171f9b8c1403842c553
alamin0561464977 <amin23435678901315@gmail.com>
2022-05-11 14:09:58 +0300

    added a Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Error404 from './assets/Error404/Error404';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Reviews from './Pages/Reviews/Reviews';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Error404 />} />
+      </Routes>
     </div>
   );
 }
