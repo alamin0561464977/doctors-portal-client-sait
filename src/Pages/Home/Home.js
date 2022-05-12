d7c58b3aa2021f3f7091de4a5829776c97ca584e
alamin0561464977 <amin23435678901315@gmail.com>
2022-05-12 08:17:47 +0300

    added a Our Services

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './Banner';
 import './Home.css'
 import InfoCard from './InfoCard';
+import OurServices from './OurServices';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <Banner></Banner>
             <InfoCard></InfoCard>
+            <OurServices></OurServices>
         </div>
     );
 };
---
+++ src/Pages/Home/OurServices.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import OurService from './OurService';
+
+const OurServices = () => {
+    const ourServices = [
+        {
+            _id: "ADC_001",
+            img: fluoride,
+            title: "Fluoride Treatment",
+            paragraph: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the"
+        },
+        {
+            _id: "ADC_002",
+            img: cavity,
+            title: "Cavity Filling",
+            paragraph: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the"
+        },
+        {
+            _id: "ADC_003",
+            img: whitening,
+            title: "Teeth Whitening",
+            paragraph: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the"
+        }
+    ];
+    return (
+        <div>
+            <div className=''>
+                <h1 className='text-center text-[#19D3AE] text-xl '><b>OUR SERVICES</b></h1>
+                <h3 className='text-center text-2xl'>Services We Provide</h3>
+            </div>
+            <div className='grid grid-cols-1 lg:grid-cols-3  gap-5 mt-5'>
+                {
+                    ourServices.map(ourService => <OurService key={ourService._id} ourService={ourService}></OurService>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default OurServices;
